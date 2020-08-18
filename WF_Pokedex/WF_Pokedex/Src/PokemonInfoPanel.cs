0603b1014e9b33eb7393392cd96cb22ab2b1cf90
Dylan Dotti <invocation42@yahoo.com>
2020-08-17 21:00:59 -0400

    Moved PokeApi.Tests to PokeApi repo

--- WF_Pokedex/WF_Pokedex/Src/PokemonInfoPanel.cs
+++ WF_Pokedex/WF_Pokedex/Src/PokemonInfoPanel.cs
@@ -1,10 +1,6 @@
 ﻿using PokeApi;
 using StringExtensions;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace WF_Pokedex
